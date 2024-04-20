13d0110fc86b2929f3a1c863b6d78b75126aadf3
tsuramii <lucanahtsu@gmail.com>
2024-04-20 20:47:06 -0300

    Improve twttr with regex

--- Week 2/twttr/twttr.mjs
+++ Week 2/twttr/twttr.mjs
@@ -2,12 +2,16 @@ import readline from "node:readline";
 import { stdin as input, stdout as output } from "node:process";
 
 const rl = readline.createInterface({ input, output });
-const VOWELS = ["a", "e", "i", "o", "u"];
-rl.question("Input: ", (input) => {
-  let stringWithoutVowels = input;
-  for (let vowel of VOWELS) {
-    stringWithoutVowels = stringWithoutVowels.replace(vowel, "");
-  }
-  console.log(stringWithoutVowels);
+const VOWELS = /[aeiou]/gi; //" REGEX: Match all occurrences of any vowel character ('a', 'e', 'i', 'o', or 'u'), regardless of case (uppercase or lowercase)."
+
+const promptUser = () => {
+  rl.question("Input: ", handleUserInput);
+};
+
+const handleUserInput = (input) => {
+  const stringWithoutVowels = input.replace(VOWELS, "");
+  rl.write(stringWithoutVowels);
   rl.close();
-});
+};
+
+promptUser();
