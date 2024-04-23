ba57fcf39ff32e5ee7f69ef658306956bea4e9e5
tsuramii <lucanahtsu@gmail.com>
2024-04-23 11:15:30 -0300

    Improve fuel

--- Week 3/fuel/fuel.mjs
+++ Week 3/fuel/fuel.mjs
@@ -3,39 +3,33 @@ import { stdin as input, stdout as output } from "process";
 
 const rl = readline.createInterface({ input, output });
 
-rl.question("Fraction: ", (input) => {
-  try {
-    const fraction = input.split("/");
-    const [numerator, denominator] = [
-      parseInt(fraction[0]),
-      parseInt(fraction[1]),
-    ];
+function calculateFuelPercentage(fraction) {
+  const [numerator, denominator] = fraction.split("/").map(Number);
 
-    if (
-      isNaN(numerator) ||
-      isNaN(denominator) ||
-      numerator.toString() !== fraction[0] ||
-      denominator.toString() !== fraction[1]
-    ) {
-      throw new Error("Please insert a valid integer fraction (e.g., 3/4)");
-    }
-
-    if (numerator < 0 || denominator <= 0 || numerator > denominator) {
-      throw new Error("Please insert a valid Numerator/Denominator");
-    }
-
-    const percentaje = (numerator / denominator) * 100;
-
-    if (percentaje === 100) {
-      console.log("F");
-    } else if (percentaje <= 1) {
-      console.log("E");
-    } else {
-      console.log(`${percentaje}%`);
-    }
-  } catch (error) {
-    console.error(error.message);
-  } finally {
-    rl.close();
+  if (
+    !Number.isInteger(numerator) ||
+    !Number.isInteger(denominator) ||
+    numerator > denominator ||
+    denominator === 0 ||
+    numerator < 0 ||
+    denominator <= 0
+  ) {
+    console.log("Invalid fraction. Please try again.");
+    return;
   }
+
+  const percentage = Math.round((numerator / denominator) * 100);
+
+  if (percentage <= 1) {
+    console.log("E");
+  } else if (percentage >= 99) {
+    console.log("F");
+  } else {
+    console.log(`${percentage}%`);
+  }
+}
+
+rl.question("Fraction: ", (fraction) => {
+  calculateFuelPercentage(fraction);
+  rl.close();
 });
