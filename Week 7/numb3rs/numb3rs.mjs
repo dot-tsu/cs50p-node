8e533e174f3093e40f5f5fcc7e6ba337710ba280
tsuramii <lucanahtsu@gmail.com>
2024-05-09 18:38:07 -0300

    Add user input

--- Week 7/numb3rs/numb3rs.mjs
+++ Week 7/numb3rs/numb3rs.mjs
@@ -1,11 +1,22 @@
+import readlinePromises from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const rl = readlinePromises.createInterface({ input, output });
+
 const validate = (ip) => {
-  const regexp = /^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/
+  const regexp =
+    /^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/;
   return regexp.test(ip);
 };
 
-const main = () => {
-  const ip = "255.255.255.255";
+const getUserInput = async () => {
+  return await rl.question("IPv4 Adress: ");
+};
+
+const main = async () => {
+  const ip = await getUserInput();
   console.log(validate(ip));
+  process.exit(1);
 };
 
 main();
