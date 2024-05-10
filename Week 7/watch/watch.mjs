dbaf96df34fc6fb759227650a78d24162bbd0113
tsuramii <lucanahtsu@gmail.com>
2024-05-10 09:22:26 -0300

    Improve watch

--- Week 7/watch/watch.mjs
+++ Week 7/watch/watch.mjs
@@ -6,10 +6,8 @@ const rl = readlinePromises.createInterface({ input, output });
 const main = async () => {
   const html = await getHTML();
   const videoSource = parse(html);
-  let youtubeLink;
   if (videoSource) {
-    youtubeLink = `https://youtu.be/${videoSource}`;
-    console.log(youtubeLink);
+    console.log(`https://youtu.be/${videoSource}`);
   } else {
     console.error("None.");
   }
@@ -19,6 +17,7 @@ const main = async () => {
 const getHTML = () => {
   return rl.question("HTML: ");
 };
+
 const parse = (html) => {
   const regex = /(?<=embed\/)([^"]+)/g;
   const videoSource = html.match(regex);
